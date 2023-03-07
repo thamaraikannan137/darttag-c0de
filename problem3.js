88ce9db0c7d074bc7f395f17a240013c593ee2f3
thamaraikannan137 <87991135+thamaraikannan137@users.noreply.github.com>
2023-03-07 23:37:37 +0530

    Update problem3.js

--- problem3.js
+++ problem3.js
@@ -1,17 +1,16 @@
 //problem3
 
 function migrateRings(n, A, B, C, steps) {
-  if(n>0 &&A>0&&B>0&&C>0){
-    if (n === 1) {
-      steps.push(`${A}->${B}`);
-    } else {
-      migrateRings(n - 1, A, C, B, steps);
-      steps.push(`${A}->${B}`);
-      migrateRings(n - 1, C, B, A, steps);
+  if (n >= 0) {
+    if (n === 0) {
+      return;
     }
+    migrateRings(n - 1, A, C, B, steps);
+    steps.push(`${n}:${A} to ${B}`);
+    migrateRings(n - 1, C, B, A, steps);
     return steps;
   }
- return steps.push(`Rings cannot be negative value`)
+  return steps.push(`Rings cannot be negative value`);
 }
 
 function solution(N, A, B, C) {
@@ -19,4 +18,4 @@ function solution(N, A, B, C) {
   migrateRings(N, A, B, C, steps);
   return steps;
 }
-console.log(solution(2,5,4,3)) //["5->3", "5->4", "3->4"]
+console.log(solution(2, 'A', 'B', 'C')); //["1:A->C", "A->B", "1:C->B"]
