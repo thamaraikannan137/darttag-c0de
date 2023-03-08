3a244be8b2e16a652f5fd89b948f1f6220aa954a
thamaraikannan137 <87991135+thamaraikannan137@users.noreply.github.com>
2023-03-08 16:12:44 +0530

    Update problem1.js

--- problem1.js
+++ problem1.js
@@ -7,8 +7,7 @@ function fibonacci(n, memo = {}) {
   if (memo[n]) {
     return memo[n];
   }
-  memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
-  console.log(memo)
+  memo[n] = fibonacci(n - 1,memo) + fibonacci(n - 2,memo);
   return memo[n];
 }
 
